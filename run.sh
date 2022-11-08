dc6949d7ba1d06204278090569a94eff89f86e9e
Gupta <ag82@cc-login1.campuscluster.illinois.edu>
2022-11-07 22:51:47 -0600

    compmha stats

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-python pre_train_wiki.py --type MHA --batch_size 8 --layers 16 --sequence_length 124 --downsampling_k 64 > output.in 2> err.in
+python pre_train_wiki.py --type CompMHA --batch_size 8 --layers 16 --sequence_length 124 --downsampling_k 64 > output.in 2> err.in
 
