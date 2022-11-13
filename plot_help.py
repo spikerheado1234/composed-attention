98944629b24ffac88959a598f97827db4ea61726
spikerheado1234 <ahangupta.96@gmail.com>
2022-11-13 11:20:55 -0600

    revert to previous learning rate schedule

--- plot_help.py
+++ plot_help.py
@@ -25,8 +25,59 @@ def plot_loss(*args):
 
     plt.legend(headings, loc='upper left')
     plt.title('Loss Curves')
-    plt.xlabel(f'step count (per {granularity})')
+    plt.xlabel(f'step count')
     plt.ylabel('loss')
+
+def plot_new_loss(*args):
+    global line_types
+    headings = []
+    line_type = 0
+    for dir, heading in args:
+        # We plot the loss and accuracy curves for each 
+        # directory supplied.
+        loss = []
+        with open(dir, "r") as f:
+            for i, line in enumerate(f):
+                split_tokens = line.split()
+                if split_tokens[0] == "Saved" or split_tokens[0] == "-----------":
+                    continue
+                loss.append(float(split_tokens[7]))
+
+        plt.plot(loss, line_types[line_type])
+        line_type += 1
+        headings.append(heading)
+
+    plt.legend(headings, loc='upper left')
+    plt.title('Loss Curves')
+    plt.xlabel(f'step count')
+    plt.ylabel('loss')
+
+def plot_new_accuracy(*args):
+    global line_types
+
+    headings = []
+    line_type = 0
+    for dir, heading in args:
+        # We plot the loss and accuracy curves for each 
+        # directory supplied.
+        accuracy = []
+        with open(dir, "r") as f:
+            for i, line in enumerate(f):
+                split_tokens = line.split()
+                if split_tokens[0] == "Saved" or split_tokens[0] == "-----------":
+                    continue
+                curr_accuracy = float(split_tokens[9])
+                accuracy.append(curr_accuracy)
+
+        plt.plot(accuracy, line_types[line_type])
+        line_type += 1
+        headings.append(heading)
+
+    plt.legend(headings, loc='upper left')
+    plt.title('Accuracy Curves')
+    plt.xlabel(f'step count')
+    plt.ylabel('Accuracy')
+
 
 def plot_accuracy(*args):
     global line_types
@@ -60,14 +111,18 @@ def plot_accuracy(*args):
 #           ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/pt_512s_8bs_train_data.txt', 'Performer-VT Semi-Composed'))
 #plot_accuracy(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/pt_train_data.txt', 'Naive Performer'),
 #           ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/ptCvt_train_data.txt', 'Performer-VT Semi-Composed'))
-plot_loss(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/lt_train_data.txt', 'LinFormer'),
-              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/pt_train_data.txt', 'PerFormer'),
-              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/ltCpt_train_data.txt', 'Lin-Perf Composed'),
-              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/vt_train_data.txt', 'Vanilla Transformer'))
+#plot_loss(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/lt_train_data.txt', 'LinFormer'),
+#              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/pt_train_data.txt', 'PerFormer'),
+#              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/ltCpt_train_data.txt', 'Lin-Perf Composed'),
+#              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/vt_train_data.txt', 'Vanilla Transformer'))
 #plot_accuracy(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/lt_train_data.txt', 'LinFormer'),
 #              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/pt_train_data.txt', 'PerFormer'),
 #              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/ltCpt_train_data.txt', 'Lin-Perf Composed'),
 #              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/attention/data/vt_train_data.txt', 'Vanilla Transformer'))
+#plot_new_loss(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/output_one.in', 'Vanilla Transformer'),
+#              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/output_two.in', 'Composed Variant'))
+plot_new_accuracy(('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/output_one.in', 'Vanilla Transformer'),
+              ('/Users/Ahan/Desktop/Ahan/UIUC/PL-FOR-NAS/output_two.in', 'Composed Variant'))
 plt.show()
 
 
