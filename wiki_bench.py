c9a0e1f8fb201372c6c83bdc5bb0219262511fbf
spikerheado1234 <ahangupta.96@gmail.com>
2022-11-14 13:41:14 -0600

    add missing files for iccp benchmarking experiment

---
+++ wiki_bench.py
@@ -0,0 +1,185 @@
+import tensorflow as tf
+from vanilla_transformer import Transformer
+import tensorflow_datasets as tfds
+import tensorflow_text
+import time
+import argparse
+from stats import Stats 
+import os
+from pre_train_wiki_loader import get_dataset, make_batches
+from constants import Constants
+from tokenization_proc import mask
+
+## Define argument parsing and help over here. ##
+
+parser = argparse.ArgumentParser(description='Train compositions of efficient Transformer Variants.')
+parser.add_argument('--type', dest='attention_type', default='MHA', choices=['MHA', 'LinMHA', 'PerfMHA', 'CompMHA'], help='The type of attention mechanism you wish to train a Transformer on. (Possible Values are: MHA, LinMHA or PerfMHA)')
+parser.add_argument('--downsampling_k', dest='downsampling_k', default=32, type=int, help='The dimension you wish to downsample the sequence length to in accordance to the LinFormer Paper.')
+parser.add_argument('--batch_size', dest='batch_size', default=64, type=int, help='the batch size used for training & inference purposes')
+parser.add_argument('--layers', dest='layers', default=4, type=int, help='the number of layers in the transformer.')
+parser.add_argument('--sequence_length', dest='sequence_length', type=int, default=128, help='the sequence length of the input to the transformer')
+parser.add_argument('--step_count', dest='num_steps', type=int, default=500000, help='the number of steps as input to pre-training.')
+parser.add_argument('--rank', dest='rank', type=int, default=1, help='The rank of the process, to distinguish output.')
+
+args = parser.parse_args()
+
+## Define global vars here. ##
+
+MAX_TOKENS = args.sequence_length
+
+### Data preparation 
+
+curr_dir = os.getcwd() + "/"
+
+train_ds = get_dataset()
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = args.batch_size
+
+## We make the batches here. ##
+train_batches = make_batches(train_ds, BUFFER_SIZE, BATCH_SIZE)
+
+## Hyperparameters ##
+num_layers = args.layers
+d_model = 512
+dff = 3072
+num_attention_heads = 8
+dropout_rate = 0.1
+rank = args.rank
+
+transformer = Transformer(
+    num_layers=num_layers,
+    d_model=d_model,
+    num_attention_heads=num_attention_heads,
+    dff=dff,
+    input_vocab_size=Constants.wiki_vocab_size,
+    target_vocab_size=Constants.wiki_vocab_size,
+    dropout_rate=dropout_rate,
+    downsampling_value=args.downsampling_k if args.attention_type == 'LinMHA' else 32, # Just default to 32 otherwise, doesn't matter since it won't be used.
+    attention_type=args.attention_type)
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=4000):
+    super().__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+learning_rate = CustomSchedule(d_model)
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                    epsilon=1e-9)
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+def accuracy_function(real, pred):
+
+  accuracies = tf.equal(real, tf.argmax(pred, axis=2))
+
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+
+  accuracies = tf.math.logical_and(mask, accuracies)
+
+  accuracies = tf.cast(accuracies, dtype=tf.float32)
+  mask = tf.cast(mask, dtype=tf.float32)
+
+  return tf.reduce_sum(accuracies) / tf.reduce_sum(mask)
+  
+
+def loss_function(real, pred):
+  mask = tf.math.logical_not(tf.math.equal(real, 0))
+  loss_ = loss_object(real, pred)
+
+  mask = tf.cast(mask, dtype=loss_.dtype)
+  loss_ *= mask
+
+  return tf.reduce_sum(loss_)/tf.reduce_sum(mask)
+
+def perplexity_function(_loss):
+
+    return tf.math.exp(_loss * -1)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.Mean(name='train_accuracy')
+train_perplexity = tf.keras.metrics.Mean(name='train_perplexity')
+
+checkpoint_path = './checkpoints/train/' + str(rank)
+
+ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+                           transformer=transformer,
+                           optimizer=optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+train_step_signature = [
+    (
+         tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+         tf.TensorSpec(shape=(None, None), dtype=tf.int64)),
+    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+]
+
+def mask_data(inp_tok):
+  global MAX_TOKENS 
+  """
+  Pads the vector inputs (of size (BATCH_SIZE, SEQUENCE LENGTH)) to ensure each
+  sequence length is standardized to MAX_TOKENS.
+  """
+  inp, tar_inp, tar_real = mask(inp_tok, MAX_TOKENS)
+  tar_inp = tar_inp[:, :-1] # Drop the end token for the Decoder Input.
+  tar_real = tar_real[:, 1:] # Drop the start token for what we compare to.
+
+  return (inp, tar_inp), tar_real
+
+def train_step(inputs, labels):
+  (inp, tar_inp) = inputs
+  tar_real = labels
+
+  (inp, tar_inp), tar_real = mask_data(inp)
+
+  with tf.GradientTape() as tape:
+    predictions, _ = transformer([inp, tar_inp],
+                                 training = True)
+    loss = loss_function(tar_real, predictions)
+    accuracy = accuracy_function(tar_real, predictions)
+
+  gradients = tape.gradient(loss, transformer.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+  train_loss(loss)
+  train_accuracy(accuracy)
+  train_perplexity(perplexity_function(loss))
+
+EPOCHS = 30
+total_steps_required = 100
+
+steps_elapsed = 0
+
+train_start = time.time()
+for epoch in range(EPOCHS):
+  start = time.time()
+  if steps_elapsed > total_steps_required:
+    break
+
+  train_loss.reset_states()
+  train_accuracy.reset_states()
+  train_perplexity.reset_states()
+
+  for (batch, (inp, tar)) in enumerate(train_batches):
+    if steps_elapsed > total_steps_required:
+      break
+    train_step(inp, tar)
+
+    steps_elapsed += 1
+
+train_end = time.time()
+
+with open(f"benchmark_results_{args.attention_type}.txt", "a+") as f:
+    f.write(f"End-To-End Training time: {train_end-train_start}, sequence length: {args.sequence_length}\n")
