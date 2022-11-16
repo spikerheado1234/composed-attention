b489d864c6c136885ed94d598f7abb3707e5ae56
spikerheado1234 <ahangupta.96@gmail.com>
2022-11-15 18:31:53 -0600

    finish fixing junk token distribution writing

--- pre_train_wiki.py
+++ pre_train_wiki.py
@@ -155,7 +155,7 @@ distribution = [0, 0, 0, 0] # 0, 0 < 10, 10 -50, > 50 -> gives number of NON-0 e
 def compute_distribution(inp):
   global distribution
 
-  count = tf.reduce_sum(tf.cast(tf.math.logical_not(tf.equal(tf.convert_to_tensor(inp.numpy()[:-1]), 0)), dtype=tf.int64))
+  count = tf.reduce_sum(tf.cast(tf.math.logical_not(tf.equal(tf.convert_to_tensor(inp.numpy()[:, :-1]), 0)), dtype=tf.int64))
 
   if count == 0:
     distribution[0] += 1
