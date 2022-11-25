a22dec91f929a688ca8db5d70b46e53ae1adcc4f
spikerheado1234 <ahangupta.96@gmail.com>
2022-11-25 16:35:53 -0600

    val ds testing

--- debug.py
+++ debug.py
@@ -3,18 +3,19 @@
 
 ## it looks like the data pipeline is a little buggy, let us debug it. ##
 
-from pre_train_wiki_loader import get_dataset, make_batches
+from pre_train_wiki_loader import get_train_ds, make_batches, get_val_ds
 
 
 
-train_ds = get_dataset()
+train_ds = get_train_ds()
+val_ds = get_val_ds()
 
 BUFFER_SIZE = 20000
 BATCH_SIZE = 32
 train_batches = make_batches(train_ds, BUFFER_SIZE, BATCH_SIZE)
 
 ## To sanity check how many iterations it takes to finish one epoch. ##
-for batch, inp in enumerate(train_batches):
+for batch, inp in enumerate(val_ds):
     print(batch)
 
 print('finished!')
