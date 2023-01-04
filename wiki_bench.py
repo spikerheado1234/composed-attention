13921bb9c34dc29a89a007c12f6bb6498a51bce4
spikerheado1234 <ahangupta.96@gmail.com>
2023-01-04 15:28:30 -0600

    measure total foward prop time

--- wiki_bench.py
+++ wiki_bench.py
@@ -173,7 +173,10 @@ def train_step(inputs, labels):
   with tf.GradientTape() as tape:
     if args.enc_only: ## We must then remove the end token from the input into the encoder.
       inp = tf.convert_to_tensor(inp.numpy()[:, :-1])
+    forward_prop_start = time.time()
     predictions, _ = masked_lm([inp, tar_inp], training = True)
+    forward_prop_end = time.time()
+    Stats.total_forward_prop_time += (forward_prop_end - forward_prop_start)
     loss = loss_object(tar_real, predictions, sample_weight=weight[:, 1:]) 
     accuracy = accuracy_function(tar_real, predictions, weight[:, 1:])
 
@@ -219,4 +222,4 @@ train_end = time.time()
 
 with open(f"benchmark_results_{args.attention_type}.txt", "a+") as f:
     f.write(f"Train Step Time: {Stats.train_step_time}, sequence length: {args.sequence_length}, downsampling value: {args.downsampling_k}, hidden_dim: {d_model}\n")
-    f.write(f"MHA {Stats.mha_time:.4f} MHA-Enc {Stats.mha_enc_time:.4f} MHA-Causal {Stats.mha_causal_time:.4f} MHA-Enc-Dec {Stats.mha_enc_dec_time:.4f} FFN {Stats.ffn_time:.4f} Embedding: {Stats.embedding_time} Downsampling {Stats.downsampling_time:.4f} Kernel-Transformation {Stats.transformation_time:.4f} Gradient Computation: {Stats.gradient_computation} Optimisation Step: {Stats.optimser_step}\n")
+    f.write(f"Forward Prop: {Stats.total_forward_prop_time} MHA {Stats.mha_time:.4f} MHA-Enc {Stats.mha_enc_time:.4f} MHA-Causal {Stats.mha_causal_time:.4f} MHA-Enc-Dec {Stats.mha_enc_dec_time:.4f} FFN {Stats.ffn_time:.4f} Embedding: {Stats.embedding_time} Downsampling {Stats.downsampling_time:.4f} Kernel-Transformation {Stats.transformation_time:.4f} Gradient Computation: {Stats.gradient_computation} Optimisation Step: {Stats.optimser_step}\n")
